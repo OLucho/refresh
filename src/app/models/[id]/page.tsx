906be565d9a644e59a154777542ae8707128301e
Luciano Orfino <luchoorfino@gmail.com>
2024-03-07 13:45:51 -0300

    updates model detail layout

--- src/app/models/[id]/page.tsx
+++ src/app/models/[id]/page.tsx
@@ -9,11 +9,11 @@ export default async function ModelPage(params, searchParams) {
   return (
     <>
       <Header />
-      <div className="mx-auto max-w-4xl py-8">
+      <div className="mx-auto flex max-w-4xl items-center justify-center gap-10 py-8">
         <div className="mb-8 flex items-center justify-center">
           <img
             alt={`${model.name} ${model?.lastName}`}
-            className="h-72 w-72 object-cover"
+            className="h-96 w-96 object-cover"
             src={model?.img}
           />
         </div>
@@ -21,17 +21,17 @@ export default async function ModelPage(params, searchParams) {
           <h1 className="text-4xl font-bold">
             {model.name} {model?.lastName}
           </h1>
-          <p className="text-gray-800">{model?.medidas}</p>
-          <p className="text-gray-800">Color de ojos: {model?.eyesColor}</p>
-          <p className="text-gray-800">Color de pelo: {model?.hairColor}</p>
+          <p className="text-gray-300">{model?.medidas}</p>
+          <p className="text-gray-300">Color de ojos: {model?.eyesColor}</p>
+          <p className="text-gray-300">Color de pelo: {model?.hairColor}</p>
           <div className="mt-4 flex items-center justify-center">
             <a
               className="flex items-center text-blue-500 hover:text-blue-700"
-              href={`https://www.instagram.com/${model.instagram}`}
+              href={model.instagram}
               rel="noopener noreferrer"
               target="_blank"
             >
-              Instagram ${model?.instagram}
+              Instagram ícono
             </a>
           </div>
         </div>
