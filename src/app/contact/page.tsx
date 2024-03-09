f276468585522249f1809ace7b35fd9c865baa92
Luciano Orfino <luchoorfino@gmail.com>
2024-03-09 16:09:48 -0300

    test video

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,15 @@
+import Video from "next-video";
+
+import video from "../../../videos/Charlotte de Witte - Pria.mp4";
 import {Header} from "../components/header";
 
 export default function ContactPage() {
   return (
     <>
       <Header subtitle="Contact" />
-      <div />
+      <div>
+        <Video src={video} />
+      </div>
     </>
   );
 }
